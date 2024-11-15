52f8dbfcee60c5e23e5a198ce12b0f32a7cd720c
Kseniya Chadovich <kseniya.chadovich@gmail.com>
2024-11-15 20:40:52 +0900

    <<

--- src/app.js
+++ src/app.js
@@ -2,39 +2,26 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const path = require('path');
 const db = require('./config/db');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// CORS configuration (allowing only specific origin)
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
 app.use(cors({
-  origin: 'http://127.0.0.1:5500', // Change this if your front-end is hosted elsewhere
+  origin: 'http://127.0.0.1:5500'
 }));
 
-// Body parsing middleware
 app.use(express.json());
-
-// Middleware for logging requests
 app.use((req, res, next) => {
   console.log(`${req.method} request to ${req.url}`);
   next();
 });
-
-// Serve static files (CSS, JS, images) from the 'public' folder
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Define route for serving the signup page at the root URL ('/')
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'signup.html')); // This will serve signup.html
-});
-
-// API routes (for backend functions like user login, signup, etc.)
 app.use('/api', routes);
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
